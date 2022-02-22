ea6d49cee5acde95b83132fd67cb89ec8751010d
Patricia Novaes <patricia.novaes@gmail.com>
2022-02-22 16:55:47 -0300

    Aula 3 - 35min

--- src/main/java/br/com/letscode/letscodedemo/servlet/Controlador.java
+++ src/main/java/br/com/letscode/letscodedemo/servlet/Controlador.java
@@ -1,8 +1,53 @@
 package br.com.letscode.letscodedemo.servlet;
 
+import br.com.letscode.letscodedemo.dao.BancoDeDados;
+import br.com.letscode.letscodedemo.modelo.Pessoa;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/controlador")
 public class Controlador extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String acao = req.getParameter("acao");
+
+        if (acao.equals("cadastrar-pessoas")) {
+            String nome = req.getParameter("nome");
+            String sobrenome = req.getParameter("sobrenome");
+            String idade = req.getParameter("idade");
+            String data = req.getParameter("data");
+
+            Pessoa pessoa = new Pessoa(nome, sobrenome, idade, data);
+            // chama método responsável
+            cadastro(pessoa);
+        } else if (acao.equals("listar-pessoas")) {
+            listar(req, resp);
+        }
+    }
+
+    public void cadastro(Pessoa pessoa) {
+
+        BancoDeDados bd = new BancoDeDados();
+        bd.salvar(pessoa);
+    }
+
+    public void listar(HttpServletRequest req, HttpServletResponse resp) {
+        BancoDeDados bd = new BancoDeDados();
+        List<Pessoa> pessoas = bd.listar();
+        req.setAttribute("pessoas", pessoas );
+        req.setAttribute("titulo", "Página de listagem de pessoas");
+
+        try {
+            req.getRequestDispatcher("WEB-INF/lista-pessoas.jsp").forward(req, resp);
+        } catch (IOException | ServletException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- target/letscode-demo-1.0-SNAPSHOT/index.jsp
+++ target/letscode-demo-1.0-SNAPSHOT/index.jsp
@@ -1,13 +1,17 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>JSP - Hello World</title>
-</head>
-<body>
-<h1><%= "Hello World!" %>
-</h1>
-<br/>
-<a href="hello-servlet">Hello Servlet</a>
-</body>
+    <head>
+        <title>JSP - Hello World</title>
+    </head>
+    <body>
+        <h1><%= "APP de Pessoas" %></h1>
+    <br/>
+        <ul>
+            <li>
+                <a href="/app/formulario.html">Cadastrar pessoa</a>
+            </li>
+        </ul>
+
+    </body>
 </html>
