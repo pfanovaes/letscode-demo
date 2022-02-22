8f103a1d824fe6ec89c392b211c1e8220b316a33
Patricia Novaes <patricia.novaes@gmail.com>
2022-02-22 15:55:43 -0300

    Aula 3 - Refatorando

---
+++ src/main/java/br/com/letscode/letscodedemo/servlet/Controlador.java
@@ -0,0 +1,8 @@
+package br.com.letscode.letscodedemo.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/controlador")
+public class Controlador extends HttpServlet {
+}
